765f46e877cb8319649597dfc7eab9dbb00e205d
navaneetsharma22 <navaneetsharma22@gmail.com>
2026-01-26 14:12:02 +0530

    Adding Data file

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-const  express = require("express");
+const express = require("express");
+const chats = require("./data/data");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
 
 
-const app =express();
 
-app.get("/" ,(req,res) => {
-    res.send("API is Running  Successfully");
-
+app.get("/", (req, res) => {
+  res.send("API is Running Successfully");
 });
 
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
 
+app.get("/api/chat/:id", (req, res) => {
+//   console.log(req.params.id);
 
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
 
+const PORT = process.env.PORT || 5000
 
-app.listen(5000,console.log("Server Started on Port 5000"));
+app.listen(5000, () => {
+ console.log(`Server Started on Port ${PORT}`);
+});
