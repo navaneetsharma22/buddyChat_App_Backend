f2ba27564bcfc545ca8367c4d2fa13ccfa9b523d
navaneetsharma22 <navaneetsharma22@gmail.com>
2026-01-29 19:46:11 +0530

    jwt token auth

--- models/userModel.js
+++ models/userModel.js
@@ -1,24 +1,37 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     pic: {
       type: String,
-      required: true,
       default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
   },
   {
-    timestamps: true, 
+    timestamps: true,
   }
 );
 
-const User = mongoose.model("User",userSchema);
+// 🔑 Compare password (login)
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
 
+// 🔐 Hash password before save (Mongoose v7+)
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+    return;
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
-
--- server.js
+++ server.js
@@ -1,37 +1,116 @@
 const express = require("express");
-const chats = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+
+const {} = require("./middleware/errorMidsleware")
 
 
 
+require("dotenv").config();
 
 
 dotenv.config();
-
 connectDB();
+
 const app = express();
-
-
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is Running Successfully");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
+
+
+
+
+// app.get("/api/chat", (req, res) => {
+//   res.send(chats);
+// });
+
+// app.get("/api/chat/:id", (req, res) => {
+// //   console.log(req.params.id);
+
+//   const singleChat = chats.find((c) => c._id === req.params.id);
+//   res.send(singleChat);
+// });
+
+
+
+app.use("/api/user", userRoutes);
+
+app.user(notFound)
+app.user(errorHandler)
+
+  
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Started on Port ${PORT}`);
 });
 
-app.get("/api/chat/:id", (req, res) => {
-//   console.log(req.params.id);
 
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
 
-const PORT = process.env.PORT || 5000
 
-app.listen(5000, () => {
- console.log(`Server Started on Port ${PORT}` .red.bold) ;
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
