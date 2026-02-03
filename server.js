115bd9a19b6cf34e701ead6efec3d8c65ff661d1
navaneetsharma22 <navaneetsharma22@gmail.com>
2026-02-03 18:19:06 +0530

    socket IO

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
 const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const chatRoutes = require("./routes/chatRoutes");
-const { notFound, errorHandler } = require("./middleware/errorMidsleware");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
@@ -17,18 +18,54 @@ app.get("/", (req, res) => {
   res.send("API is running successfully");
 });
 
-// Chat routes
+// Routes
 app.use("/api/chat", chatRoutes);
-
-// User routes
 app.use("/api/user", userRoutes);
+app.use("/api/message", messageRoutes);
 
 // Error handling
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
+});
+
+// ================= SOCKET.IO =================
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:5173", // Vite frontend
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User joined room:" +  room);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+   var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return;
+
+    chat.users.forEach((user) => {
+      if (user._id === newMessage.sender._id) return;
+      socket.in(user._id).emit("message recieved", newMessage);
+    });
+  });
 });
